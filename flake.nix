f19191dbc92f4ec735917ad889cd43337b4155c3
Felix Ding <felixding1989@gmail.com>
2026-03-09 22:57:18 -0700

    scaffolding for http server

--- flake.nix
+++ flake.nix
@@ -21,31 +21,64 @@
           inherit system;
           overlays = [ zig-overlay.overlays.default ];
         };
+
         nativeBuildInputs = [ pkgs.zigpkgs."0.15.2" ];
+
+        # Because we are building this for raspberry pi 5
         zigTarget = "aarch64-linux-gnu";
+
         mkZigPkg =
           { name, src }:
           let
             stdenvFor =
               if system == "x86_64-linux" then pkgs.pkgsCross.aarch64-multiplatform.stdenv else pkgs.stdenv;
+
+            zigDeps = import pkgs/${name}/build.zig.zon.nix {
+              inherit (pkgs)
+                lib
+                linkFarm
+                fetchurl
+                fetchgit
+                runCommandLocal
+                ;
+              inherit (pkgs) zig; # we might need to change this to keep it consistent with what is specified in nativeBuildInputs
+              name = "zig-packages";
+            };
           in
           stdenvFor.mkDerivation {
             pname = name;
             version = "0.0.0";
+
             inherit src nativeBuildInputs;
+
+            preBuild = ''
+              export ZIG_GLOBAL_CACHE_DIR=$PWD/zig-cache # might also need to change this since it looks like every pkg folder has their own zig cache
+              # Set up Zig dependency cache
+              mkdir -p $ZIG_GLOBAL_CACHE_DIR/p
+              find ${zigDeps} -maxdepth 1 -type l | while read dep; do
+                ln -sf $(readlink "$dep") "$ZIG_GLOBAL_CACHE_DIR/p/$(basename "$dep")"
+              done
+            '';
+
             buildPhase = ''
+              runHook preBuild
               export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-cache"
               export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-cache-local"
               mkdir -p "$ZIG_GLOBAL_CACHE_DIR" "$ZIG_LOCAL_CACHE_DIR"
               zig build -Dtarget=${zigTarget} -Doptimize=ReleaseSafe -p "$out"
             '';
+
             installPhase = "true";
           };
+
         pkgsDir = ./pkgs;
+
         pkgsEntries = builtins.readDir pkgsDir;
+
         pkgNames = builtins.filter (
           name: pkgsEntries.${name} == "directory" && builtins.pathExists (pkgsDir + "/${name}/build.zig")
         ) (builtins.attrNames pkgsEntries);
+
         zigPackages = builtins.listToAttrs (
           map (name: {
             inherit name;
@@ -55,6 +88,7 @@
             };
           }) pkgNames
         );
+
         defaultPkgName =
           if builtins.hasAttr "main_compute" zigPackages then
             "main_compute"
@@ -62,12 +96,14 @@
             builtins.head pkgNames
           else
             null;
+
         buildApp = pkgs.writeShellApplication {
           name = "waterbot-build";
           text = ''
             exec ${./scripts/deploy-core.sh} build "$@"
           '';
         };
+
         deployApp = pkgs.writeShellApplication {
           name = "waterbot-deploy";
           text = ''
