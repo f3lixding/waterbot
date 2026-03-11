247167925a8e56214314c3f98941885538bbb4f9
Felix Ding <felixding1989@gmail.com>
2026-03-10 21:35:47 -0700

    adds build logic for stand alone http server test entry point

--- pkgs/main_compute/build.zig
+++ pkgs/main_compute/build.zig
@@ -1,8 +1,19 @@
 const std = @import("std");
 
+const SharedDeps = struct {
+    import_name: []const u8,
+    module_name: []const u8,
+    dep: *std.Build.Dependency,
+};
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const httpz = b.dependency("httpz", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     const main_bin = b.addExecutable(.{
         .name = "main_compute",
@@ -13,5 +24,31 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const shared: []const SharedDeps = &[_]SharedDeps{
+        .{ .import_name = "httpz", .module_name = "httpz", .dep = httpz },
+    };
+
+    addSharedDeps(main_bin, shared[0..]);
+
     b.installArtifact(main_bin);
+
+    // This is to offer an easy way to test the http server
+    const test_bin = b.addExecutable(.{
+        .name = "test_bin",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/Server.zig"),
+            .optimize = optimize,
+            .target = target,
+        }),
+    });
+
+    addSharedDeps(test_bin, shared);
+
+    b.installArtifact(test_bin);
+}
+
+fn addSharedDeps(c: *std.Build.Step.Compile, deps: []const SharedDeps) void {
+    for (deps) |d| {
+        c.root_module.addImport(d.import_name, d.dep.module(d.module_name));
+    }
 }
---
+++ pkgs/main_compute/src/Server.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const httpz = @import("httpz");
+
+const Allocator = std.mem.Allocator;
+const PORT: u16 = 8888;
+
+pub fn run(allocator: Allocator) !void {
+    var server = try httpz.Server(void).init(allocator, .{
+        .address = .all(PORT),
+    }, {});
+    defer server.deinit();
+    defer server.stop();
+
+    var router = try server.router(.{});
+    router.get("/home", serveHome, .{});
+
+    std.debug.print("listening http://0.0.0.0:{d}/\n", .{PORT});
+    try server.listen();
+}
+
+fn serveHome(_: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    res.body =
+        \\<!DOCTYPE html>
+        \\<html>
+        \\  <head>
+        \\    <meta charset="utf-8" />
+        \\    <title>Waterbot</title>
+        \\  </head>
+        \\  <body>
+        \\    <h1>Waterbot</h1>
+        \\    <p>Home page placeholder.</p>
+        \\  </body>
+        \\</html>
+    ;
+}
+
+pub fn main() void {
+    const allocator = std.heap.page_allocator;
+    run(allocator) catch unreachable;
+}
